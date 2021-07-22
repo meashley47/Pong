b6988a615b7c4744eafcb1afac7b30d81d297e59
Ashley Thomas <avthomas@usc.edu>
2021-07-21 17:42:27 -0700

    Paddle speeds updated

--- Opponent.gd
+++ Opponent.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed = 400
+export var speed = 500
 onready var ball
 
 func _ready():
--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed = 400
+export var speed = 500
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
